efd977891592f72c6273dbff7b292e52c741c944
ryan hs <mr.ryansilalahi@gmail.com>
2017-08-29 03:27:25 +0700

    implement module along with index.ts

---
+++ public/ng/src/app/modules/about/about.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// material.angular.io
+import { MdNativeDateModule } from '@angular/material';
+import { MaterialAngularIOModule } from './../../glues/material.angular.io/';
+import 'hammerjs';
+
+// common
+import { SharedComponentsModule } from './../../shared-components/';
+
+// components
+import { PageComponent } from './page/page.component';
+
+
+@NgModule({
+	imports: [
+		CommonModule,
+		MaterialAngularIOModule,
+		SharedComponentsModule,
+		RouterModule.forChild([
+			{
+				path: '',
+				component: PageComponent,
+				children: [
+
+				]
+			}
+		] as Routes)
+	],
+	declarations: [
+		PageComponent
+	],
+})
+export class AboutModule { }
---
+++ public/ng/src/app/shared-components/shared-components.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+// material.angular.io
+import { MdNativeDateModule } from '@angular/material';
+import { MaterialAngularIOModule } from './../glues/material.angular.io/';
+import 'hammerjs';
+
+// components
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+	imports: [
+		CommonModule,
+        RouterModule,
+		MaterialAngularIOModule
+    ],
+	exports: [
+        NavbarComponent
+    ],
+	declarations: [
+		NavbarComponent
+	],
+})
+export class SharedComponentsModule { }
